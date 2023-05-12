bb5d5575b4fa8c5790c8e81824a8caf544dd5726
Malik Muhammad Usman <usmaan.maalik9503@gmail.com>
2023-05-12 20:40:54 +0500

    factorial without using loop

--- factorial.js
+++ factorial.js
@@ -1,13 +1,25 @@
 // Implement a function named factorial that has one parameter: an integer, . It must return the value of  (i.e.,  factorial).
 
 
-function factorial(n){
-    var value = 1
-     for(let a=1; a<=n; a++){
-     value = value * a
-     }
-    return value
+function factorial(n) {
+  var value = 1
+  for (let a = 1; a <= n; a++) {
+    value = value * a
   }
-  
-  
-  factorial(6)
+  return value
+}
+
+factorial(6)
+
+
+//factorial withou using loop
+function decrementNumber(number) {
+  if (number == 1) {
+    return 1;
+  } else {
+    return number * decrementNumber(number - 1);
+  }
+}
+
+console.log(decrementNumber(6));
+
