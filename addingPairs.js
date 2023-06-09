543ce5d476f4730d9601aec48d70065884f774e3
Malik Muhammad Usman <usmaan.maalik9503@gmail.com>
2023-06-09 16:36:46 +0500

    add pairing algorithm

--- addingPairs.js
+++ addingPairs.js
@@ -5,13 +5,13 @@
 // 3: output the same combo pair only once (removes the reversed ordered pairs).
 
 
-// For example passing in [1, 1, 2, 4, 4, 5, 5, 5, 6, 7, 9] the following r
 // For example passing in [1, 1, 2, 4, 4, 5, 5, 5, 6, 7, 9] the following results should occur:
 // 1) output all pairs would output: [1,9], [1,9], [4,6], [4,6], [5,5], [5,5], [5,5], [5,5], [5,5], [5,5], [6,4], [6,4] [9,1] , [9,1]
 // 2) output unique pairs only once would output: [1,9], [4,6], [5,5], [6,4], [9,1]
 // 3) output the same combo pair only once would output: [1,9], [4,6], [5,5]
 
 
+//PART 1
 function Pairs(arr) {
   var result = []
   for (let i = 0; i < arr.length; i++) {
@@ -27,3 +27,43 @@ function Pairs(arr) {
 }
 
 console.log(Pairs([1, 1, 2, 4, 4, 5, 5, 5, 6, 7, 9]))
+
+
+//PART 2
+function findUniquePairs(array) {
+  const pairs = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === 10) {
+        pairs.add(JSON.stringify([array[i], array[j]]));  //JSON.stringify convert javascript object to string
+        pairs.add(JSON.stringify([array[j], array[i]]));
+      }
+    }
+  }
+
+  return Array.from(pairs).map(JSON.parse);
+}
+
+
+console.log(findUniquePairs([1, 1, 2, 4, 4, 5, 5, 5, 6, 7, 9]))
+
+
+//PART 3
+function findComboPairs(array) {
+  const pairs = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === 10) {
+        const pair = [array[i], array[j]]
+        pairs.add(JSON.stringify(pair));
+      }
+    }
+  }
+
+  return Array.from(pairs).map(JSON.parse);
+}
+
+
+console.log(findComboPairs([1, 1, 2, 4, 4, 5, 5, 5, 6, 7, 9]))
